b9101ccfff3679e6da7d452187ef509c8087773c
Dren <drenmyrtezani@gmai.com>
2025-11-03 18:12:15 +0100

    init

--- src/main/java/at/ac/hcw/chess/Main.java
+++ src/main/java/at/ac/hcw/chess/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true); // Fullscreen aktivieren
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
